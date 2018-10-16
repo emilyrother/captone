394a347dc0d8af78d03345c9ee3e61de329eb08b
emilyrother <emily.rother@gmail.com>
2018-10-16 09:21:32 -0400

    testing

---
+++ src.py
@@ -0,0 +1,13 @@
+import cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('sample.tif',0)
+edges = cv.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
